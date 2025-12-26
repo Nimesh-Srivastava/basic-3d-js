608ee64c393681b05626cf04f9de44dc80035517
Nimesh Srivastava <snimesh412@gmail.com>
2025-12-26 23:16:03 +0530

    render 2 faces of cube

--- index.js
+++ index.js
@@ -14,12 +14,23 @@ function clear() {
   ctx.fillRect(0, 0, game.width, game.height);
 }
 
+// define a vertex
 function point({ x, y }) {
   const s = 20;
   ctx.fillStyle = FOREGROUND;
   ctx.fillRect(x - s / 2, y - s / 2, s, s);
 }
 
+// define a line
+function line(p1, p2) {
+  ctx.strokeStyle = FOREGROUND;
+  ctx.beginPath();
+  ctx.moveTo(p1.x, p1.y);
+  ctx.lineTo(p2.x, p2.y);
+  ctx.stroke();
+}
+
+// put a 2d point on canvas
 function display(p) {
   return {
     x: ((p.x + 1) / 2) * game.width,
@@ -27,6 +38,7 @@ function display(p) {
   };
 }
 
+// project 3d point in 2d plane
 function project({ x, y, z }) {
   return {
     x: x / z,
@@ -36,10 +48,12 @@ function project({ x, y, z }) {
 
 const dt = 1 / FPS;
 
+// traverse along z axis
 function translate_z({ x, y, z }, dz) {
   return { x, y, z: z + dz };
 }
 
+// rotate along y axis
 function rotate_y({ x, y, z }, theta) {
   const s = Math.sin(theta);
   const c = Math.cos(theta);
@@ -51,29 +65,51 @@ function rotate_y({ x, y, z }, theta) {
   };
 }
 
+// define all vertices
 const vs = [
   { x: 0.25, y: 0.25, z: 0.25 },
   { x: -0.25, y: 0.25, z: 0.25 },
-  { x: 0.25, y: -0.25, z: 0.25 },
   { x: -0.25, y: -0.25, z: 0.25 },
+  { x: 0.25, y: -0.25, z: 0.25 },
 
   { x: 0.25, y: 0.25, z: -0.25 },
   { x: -0.25, y: 0.25, z: -0.25 },
-  { x: 0.25, y: -0.25, z: -0.25 },
   { x: -0.25, y: -0.25, z: -0.25 },
+  { x: 0.25, y: -0.25, z: -0.25 },
+];
+
+// define all edges
+const eds = [
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
 ];
 
 let dz = 1;
 let angle = 0;
 
+// rendering function
 function frame() {
   // dz += 1 * dt;
   angle += Math.PI * dt;
 
+  // clear the canvas from provious frame
   clear();
 
+  // render the vertices
   for (const v of vs) {
     point(display(project(translate_z(rotate_y(v, angle), dz))));
+  }
+
+  // render the edges
+  for (const e of eds) {
+    for (let i = 0; i < e.length; ++i) {
+      const a = vs[e[i]];
+      const b = vs[e[(i + 1) % e.length]];
+      line(
+        display(project(translate_z(rotate_y(a, angle), dz))),
+        display(project(translate_z(rotate_y(b, angle), dz))),
+      );
+    }
   }
 
   setTimeout(frame, 1000 / FPS);
