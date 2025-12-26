da43931c072e11d6eb503796d7d03e227b0a08a5
Nimesh Srivastava <snimesh412@gmail.com>
2025-12-26 22:05:50 +0530

    convert coordinates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const BACKGROUND = "black";
-const FOREGROUND = "green";
+const FOREGROUND = "yellow";
 
 console.log(game);
 game.width = 800;
@@ -13,11 +13,18 @@ function clear() {
   ctx.fillRect(0, 0, game.width, game.height);
 }
 
-function point(x, y) {
+function point({ x, y }) {
   const s = 10;
   ctx.fillStyle = FOREGROUND;
   ctx.fillRect(x, y, s, s);
 }
 
+function project(p) {
+  return {
+    x: ((p.x + 1) / 2) * game.width,
+    y: ((p.y + 1) / 2) * game.height,
+  };
+}
+
 clear();
-point(100, 100);
+point(project({ x: 0, y: 0 }));
